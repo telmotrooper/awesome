cd928045cb033b782e5acdf2ebf2616b51154185
Telmo Henrique Valverde <telmo.trooper@gmail.com>
2019-01-14 21:39:48 -0200

    Moved Zenburn theme to awesome folder

--- rc.lua
+++ rc.lua
@@ -57,7 +57,7 @@ end
 
 -- {{{ Variable definitions
 -- Themes define colours, icons, font and wallpapers.
-beautiful.init(gears.filesystem.get_themes_dir() .. "zenburn/theme.lua")
+beautiful.init("~/.config/awesome/zenburn/theme.lua")
 
 beautiful.useless_gap = 5
 
